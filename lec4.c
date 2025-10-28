95feb0d90fc237c9cf957d1ad88b27700686ea9c
divyachadar5-eng <divyachadar5@gmail.com>
2025-10-28 19:20:02 +0530

    Add files via upload

---
+++ lec4.c
@@ -0,0 +1,102 @@
+//Que.write acode using by switch case.
+//  #include <stdio.h>
+// int main()
+// {
+//     int a=3;
+//     switch(a)
+// {
+//     case 1:
+//     printf("hello");
+//     break;
+//     case 2:
+//     printf("hiii");
+//     break;
+//     default:
+//     printf("shree");
+// }
+// return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     int a=1;
+//     int b=2;
+//     int c=3;
+//     switch(a)
+//     {
+//         case 1:
+//         printf("monday");
+//         break;
+//         case 2:
+//         printf("tuesday");
+//         break;
+//         case 3:
+//         printf("wednesday");
+//         break;
+//         case 4:
+//         printf("thursday");
+//         break;
+//         case 5:
+//         printf("friday");
+//         break;
+//         case 6:
+//         printf("saturday");
+//         break;
+//         case 7:
+//         printf("sunday");
+//         break;
+//         default:
+//         printf("shree");
+//         break;
+//     }
+//     return 0;
+// }
+
+#include <stdio.h>
+int main(){
+    int a=1;
+    switch(a)
+    {
+        case 1:
+        printf("January");
+        break;
+        case 2:
+        printf("February");
+        break;
+        case 3:
+        printf("March");
+        break;
+        case 4:
+        printf("April");
+        break;
+        case 5:
+        printf("May");
+        break;
+        case 6:
+        printf("June");
+        break;
+        case 7:
+        printf("July");
+        break;
+        case 8:
+        printf("August");
+        break;
+        case 9:
+        printf("September");
+        break;
+        case 10:
+        printf("October");
+        break;
+        case 11:
+        printf("Nobember")
+        break;
+        case 12:
+        printf("December");
+        break;
+        default:
+        printf("Kuch nhi");
+        break;
+    }
+    return 0;
+}
